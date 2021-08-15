294b3dba30360c2c84e4e8d34cd63e9d2998c005
ak99372 <andrej.developer@gmail.com>
2021-08-15 13:52:37 -0700

    Show router in console

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,7 +1,12 @@
 import { Component, VERSION } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'hello-cmp',
   template: '<p> Hellow Component</p>'
 })
-export class HelloComponent {}
+export class HelloComponent {
+  constructor(router: Router) {
+    //console.log(`This is router`, router);
+  }
+}
